30e06d574b16757ac2789448718591144891b937
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-03-19 16:41:57 +0900

    Refactor: replace local var to getTotalCharge

--- VideoRentalShop/after/src/Customer.java
+++ VideoRentalShop/after/src/Customer.java
@@ -26,7 +26,7 @@ class Customer {
 		while (rentals.hasMoreElements()) {
 			Rental each = (Rental) rentals.nextElement();
 			// add frequent renter points
-			frequentRenterPoints += getFrequentRenterPoints(each);
+			frequentRenterPoints += each.getFrequentRenterPoints();
 
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
@@ -38,13 +38,5 @@ class Customer {
 		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 
 		return result;
-	}
-
-	private int getFrequentRenterPoints(Rental each) {
-		
-		// add bonus for a two day new release rental
-		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-			return 2;
-		return 1;
 	}
 }
