08003b1a0b18e3d5a196986992304e99810eccde
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-03-19 18:02:29 +0900

    Refactor: Separation of source files(public class NewReleasePrice extends Price {)

--- VideoRentalShop/after/src/Price.java
+++ VideoRentalShop/after/src/Price.java
@@ -1,31 +1,40 @@
 public abstract class Price {
 	public abstract int getPriceCode() ; 
-	public double getCharge(int daysRented) {
-		double result = 0; 
-		switch (getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (daysRented > 2)
-				result += (daysRented - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += daysRented * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (daysRented > 3)
-				result += (daysRented - 3) * 1.5;
-			break;
-		}
-		return result;
-	}
-}
-public class NewReleasePrice extends Price {
-	public int getPriceCode() { return Movie.NEW_RELEASE ; } 
+	public abstract double getCharge(int daysRented) ;
+	public int getFrequentRenterPoints(int daysRented) { return 1 ; }
+//	public double getCharge(int daysRented) {
+//		double result = 0; 
+//		switch (getPriceCode()) {
+//		case Movie.REGULAR:
+//			result += 2;
+//			if (daysRented > 2)
+//				result += (daysRented - 2) * 1.5;
+//			break;
+//		case Movie.NEW_RELEASE:
+//			result += daysRented * 3;
+//			break;
+//		case Movie.CHILDRENS:
+//			result += 1.5;
+//			if (daysRented > 3)
+//				result += (daysRented - 3) * 1.5;
+//			break;
+//		}
+//		return result;
+//	}
 }
 public class RegularPrice extends Price {
 	public int getPriceCode() { return Movie.REGULAR ; } 
+	public double getCharge(int daysRented) {
+		double result =2;
+		if (daysRented > 2) result += (daysRented - 2) * 1.5;
+		return result;
+	} 
 }
 public class ChildrenPrice extends Price {
 	public int getPriceCode() { return Movie.CHILDRENS ; } 
-}
+	public double getCharge(int daysRented) {
+		double result =1.5;
+		if (daysRented > 3) result += (daysRented - 3) * 1.5; 
+		return result ;
+	} 	
+} 
