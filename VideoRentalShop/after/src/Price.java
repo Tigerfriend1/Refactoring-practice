7ee5682f219b0c71bf1140235d74a6c83d45e8f8
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-03-19 18:06:42 +0900

    Refactor: Separation of source files(RegularPrice class)

--- VideoRentalShop/after/src/Price.java
+++ VideoRentalShop/after/src/Price.java
@@ -22,14 +22,6 @@ public abstract class Price {
 //		return result;
 //	}
 }
-public class RegularPrice extends Price {
-	public int getPriceCode() { return Movie.REGULAR ; } 
-	public double getCharge(int daysRented) {
-		double result =2;
-		if (daysRented > 2) result += (daysRented - 2) * 1.5;
-		return result;
-	} 
-}
 public class ChildrenPrice extends Price {
 	public int getPriceCode() { return Movie.CHILDRENS ; } 
 	public double getCharge(int daysRented) {
